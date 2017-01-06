4b6fcf3827d8c3d5feb7daf54d279db7edceb6ea
Your Name <you@example.com>
2017-01-06 13:27:53 +0000

    1st commit

---
+++ graphvizml.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name = 'graphvizml'
+  s.version = '0.1.0'
+  s.summary = 'Generates a PNG file from GraphViz using a GraphViz Markup Language file'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/graphvizml.rb']
+  s.add_runtime_dependency('rexle', '~> 1.4', '>=1.4.3')
+  s.add_runtime_dependency('ruby-graphviz', '~> 1.2', '>=1.2.2')
+  s.signing_key = '../privatekeys/graphvizml.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@jamesrobertson.eu'
+  s.homepage = 'https://github.com/jrobertson/graphvizml'
+end
---
+++ lib/graphvizml.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+# file: graphvizml.rb
+
+
+require 'rexle'
+require 'graphviz'  # this loads the ruby-graphviz gem
+
+class GraphVizML
+
+
+  def initialize(filename)
+
+    @g = GraphViz::new( "structs", "type" => "graph" )
+    @g[:rankdir] = "LR"
+    @filename = filename
+    @doc = Rexle.new(File.read filename)
+
+  end
+
+  def to_png(filename=@filename.sub(/\.xml$/,'.png'))
+
+    e_options = @doc.root.element 'options'
+    e_nodes = @doc.root.element 'nodes'
+    e_edges = @doc.root.element 'edges'
+
+
+    # set global node options
+
+    xpath_node = "records/option[summary/type='node']/records/attribute"
+    e_options.root.xpath(xpath_node).each do |attribute|
+      @g.node[attribute.text('name').to_sym] = attribute.text('value')
+    end
+
+    xpath_edge = "records/option[summary/type='edge']/records/attribute"
+    e_options.root.xpath(xpath_edge).each do |attribute|
+      @g.edge[attribute.text('name').to_sym] = attribute.text('value')
+    end
+
+    # add the nodes
+
+    e_nodes.root.xpath('records/node').each do |node|
+      @g.add_node(node.attribute('id').to_s).label = node.text('label') + '1'
+    end
+
+    # add the edges
+
+    e_edges.root.xpath('records/edge').each do |edge|
+      a = edge.xpath('records/node')
+      @g.add_edge(a[0].attribute('id').to_s, 
+        a[1].attribute('id').to_s).label = edge.text('summary/label').to_s
+    end
+
+    # output the file
+
+    out_type = e_edges.root.text('summary/output_type')
+    out_file = e_edges.root.text('summary/output_file')
+    @g.output( png: filename )
+  end
+
+end
